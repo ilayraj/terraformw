adcdbb58113ee26b03d1853652e17e3d74d57f3a
ilayraj <47520155+ilayraj@users.noreply.github.com>
2025-10-19 06:52:46 +0530

    Add Terraform configuration for local file resource

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1"
+    }
+  }
+}
+
+resource "local_file" "test_file" {
+  filename = "example.txt"
+  content  = "Hello from Terraform in GitHub Actions!"
+}
+
+output "file_path" {
+  value = local_file.test_file.filename
+}
